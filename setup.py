263a958fc55c223adac58cadf4ee2d4ec4d703f4
multi-taskk <143498304+multi-taskk@users.noreply.github.com>
2023-08-29 14:46:27 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='multi_taskk',
+    version='0.1',
+    description='A multi tasking library for python',
+    author='multi-taskk',
+    packages=['multi-taskk'],
+)
